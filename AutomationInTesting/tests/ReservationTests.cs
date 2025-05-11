60e6370155e55f5f3db9007eee717b784eb73007
ShenayRamandh <ramandh.s@gmail.com>
2025-05-11 23:22:25 +0200

    Refactor and extend reservation handling tests
    Replaced "SimpleTest" with "ReservationTests" to add comprehensive test cases for reservation form validation. Refactored the Reservation repository class to improve robustness and added functionality for handling user inputs, button interactions, and error scenarios.

---
+++ AutomationInTesting/tests/ReservationTests.cs
@@ -0,0 +1,80 @@
+﻿using AutomationInTesting.Infrastructure;
+using NUnit.Framework;
+
+namespace AutomationInTesting.tests;
+
+[TestFixture]
+public class ReservationTests : TestBase
+{
+    [Test]
+    public void Reservation_GiveInvalidFirstName_ShouldThrowException()
+    {
+        // Arrange
+        Repository.NavigateToUrl(BaseUrl);
+        Repository.Reservation.ClickBookNowButton();
+        Repository.Reservation.ClickReserveButton();
+        
+        // Act
+        Repository.Reservation.EnterFirstName();
+        Repository.Reservation.EnterLastName("James");
+        Repository.Reservation.EnterEmail("James@gmail.com");
+        Repository.Reservation.EnterPhone("1234567890");
+        Repository.Reservation.ClickReserveNowButton();
+        
+        // Assert
+    }
+    
+    [Test]
+    public void Reservation_GiveInvalidLastName_ShouldThrowException()
+    {
+        // Arrange
+        Repository.NavigateToUrl(BaseUrl);
+        Repository.Reservation.ClickBookNowButton();
+        Repository.Reservation.ClickReserveButton();
+        
+        // Act
+        Repository.Reservation.EnterFirstName("James");
+        Repository.Reservation.EnterLastName();
+        Repository.Reservation.EnterEmail("James@gmail.com");
+        Repository.Reservation.EnterPhone("1234567890");
+        Repository.Reservation.ClickReserveNowButton();
+        
+        // Assert
+    }
+    
+    [Test]
+    public void Reservation_GiveInvalidEmailAddress_ShouldThrowException()
+    {
+        // Arrange
+        Repository.NavigateToUrl(BaseUrl);
+        Repository.Reservation.ClickBookNowButton();
+        Repository.Reservation.ClickReserveButton();
+        
+        // Act
+        Repository.Reservation.EnterFirstName("James");
+        Repository.Reservation.EnterLastName("Jeff");
+        Repository.Reservation.EnterEmail();
+        Repository.Reservation.EnterPhone("1234567890");
+        Repository.Reservation.ClickReserveNowButton();
+        
+        // Assert
+    }
+    
+    [Test]
+    public void Reservation_GiveInvalidPhoneNumber_ShouldThrowException()
+    {
+        // Arrange
+        Repository.NavigateToUrl(BaseUrl);
+        Repository.Reservation.ClickBookNowButton();
+        Repository.Reservation.ClickReserveButton();
+        
+        // Act
+        Repository.Reservation.EnterFirstName("James");
+        Repository.Reservation.EnterLastName("Jeff");
+        Repository.Reservation.EnterEmail("James@gmail.com");
+        Repository.Reservation.EnterPhone();
+        Repository.Reservation.ClickReserveNowButton();
+        
+        // Assert
+    }
+}

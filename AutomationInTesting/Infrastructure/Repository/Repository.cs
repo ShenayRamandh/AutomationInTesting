c2adf6e5dd9289c9fd115f4c53deea2548a3cbe4
ShenayRamandh <ramandh.s@gmail.com>
2025-05-11 21:55:39 +0200

    Add initial test setup with Selenium and NUnit integration
    This commit introduces a foundational test infrastructure, including `TestBase`, a Selenium-powered repository pattern, and a simple test for navigating the AutomationInTesting homepage. Key dependencies such as FluentAssertions, NUnit, and Selenium packages have been added to support testing.

---
+++ AutomationInTesting/Infrastructure/Repository/Repository.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace AutomationInTesting.Infrastructure.Repository;
+
+public class Repository(IWebDriver driver)
+{
+    public void NavigateToUrl(string url)
+    {
+        driver.Navigate().GoToUrl(url);
+    }
+    
+    public void ClickBookNowButton()
+    {
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+  
+        // Wait for the element to be present
+        var bookNowButton = wait.Until(driverInstance => driverInstance.FindElement(By.LinkText("Book now")));
+  
+        // Scroll the element into view
+        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", bookNowButton);
+
+        // Perform a JS click to bypass click interception
+        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", bookNowButton);
+    }
+
+}
---
+++ AutomationInTesting/tests/SimpleTest.cs
@@ -0,0 +1,16 @@
+﻿using AutomationInTesting.Infrastructure;
+using NUnit.Framework;
+
+namespace AutomationInTesting.tests;
+
+[TestFixture]
+public class SimpleTest : TestBase
+{
+    [Test]
+    public void ShouldLaunchAutomationTestingHomePage()
+    {
+        Repository.NavigateToUrl(BaseUrl);
+        Repository.ClickBookNowButton();
+    }
+
+}
